da54202c2d6ebb2e2d6a9941a5b29a4164f1b8a1
dingwanyu <dingwanyu@hunisoft.com>
2019-07-30 16:44:07 +0800

    2019-7-30 axios init dwy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './app/main.js')],
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './src/main.js')],
     output: {
         path: path.resolve(__dirname, './build'),
         filename: 'bundle.js',
