e3bb021885984f50b94712c432e9ac048cad1ecf
dingwanyu <dingwanyu@hunisoft.com>
2019-08-28 15:04:02 +0800

    2019-8-28 Demo1 update to antd

--- src/view/Demo1.js
+++ src/view/Demo1.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Table, Tag, Pagination, Button, Card, Dialog, Form, Input, Message, MessageBox } from 'element-react'
-export default class Demo1 extends React.Component {
-    constructor(props) {
-        super(props);
+import { MessageBox } from 'element-react'
+import { Table, Tag, Button, Modal, Form, Input, Icon, Card, message } from 'antd';
 
+class Demo1 extends React.Component {
+    constructor(props, context) {
+        super(props, context);
         this.state = {
             columns: [
                 {
-                    type: 'selection',
-                    label: ""
-                },
-                {
-                    label: "序号",
-                    prop: "id",
+                    title: "序号",
+                    key: "id",
+                    dataIndex: 'id',
                     width: 180
                 },
                 {
-                    label: "用户名",
-                    prop: "username",
+                    title: "用户名",
+                    key: "username",
+                    dataIndex: 'username',
                     width: 180
                 },
                 {
-                    label: "密码",
-                    prop: "password",
+                    title: "密码",
+                    key: "password",
                     render: function (data) {
                         return <Tag>{data.password}</Tag>
                     }
@@ -31,85 +30,126 @@ export default class Demo1 extends React.Component {
             ],
             data: [],
             page: {
-                total: 0
+                total: 0,
+                pageSize: 0
             },
             dialogVisible: false,
             form: {
+                id: null,
                 username: '',
                 password: ''
             },
             selection: [],
-            state: ''
+            state: '',
         }
     }
 
     render() {
+        /**
+         * 初始化form getFieldDecorator方法
+         * @author dwy
+         */
+        const { getFieldDecorator } = this.props.form;
+        /**
+         * 初始化列表选中方法
+         * @author dwy
+         */
+        const rowSelection = {
+            onChange: (selectedRowKeys, selectedRows) => {
+                this.setState({ selection: selectedRows })
+            },
+        }
+        /**
+         * 初始化分页参数
+         * @author dwy
+         */
+        const paginationProps = {
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: () => `共${this.state.page.total}条`,
+            pageSize: this.state.page.pageSize,
+            // current: page.pageNum,
+            total: this.state.page.total,
+            onShowSizeChange: (current, pageSize) => this.changePageSize(pageSize, current),
+            onChange: (current) => this.changePage(current),
+        };
         return (
             <div>
                 <Card className="box-card">
                     <div style={{ marginBottom: "10px" }}>
-                        <Button type="info" onClick={this.createUser.bind(this)}>新增</Button>
-                        <Button type="success" onClick={this.updateUser.bind(this)}>修改</Button>
-                        <Button type="danger" onClick={this.deleteUser.bind(this)}>删除</Button>
+                        <Button style={{ marginLeft: "5px" }} type="primary" onClick={this.createUser.bind(this)}>新增</Button>
+                        <Button style={{ marginLeft: "5px" }} type="primary" onClick={this.updateUser.bind(this)}>修改</Button>
+                        <Button style={{ marginLeft: "5px" }} type="danger" onClick={this.deleteUser.bind(this)}>删除</Button>
                     </div>
-                    {/* <Card style={{ marginTop: "10px" }} className="box-card"> */}
                     <Table
+                        rowKey={record => record.id}
                         style={{ width: '100%' }}
                         columns={this.state.columns}
                         maxHeight={200}
-                        data={this.state.data}
-                        onSelectChange={(selection) => { this.setState({ selection: selection }) }}
+                        dataSource={this.state.data}
+                        rowSelection={rowSelection}
+                        pagination={paginationProps}
                     />
-
-                    <Pagination style={{ float: "right" }} layout="total, sizes, prev, pager, next, jumper" total={this.state.page.total} pageSizes={[11, 30, 50, 100]} pageSize={10} currentPage={1} />
                 </Card>
                 {/* </Card> */}
-                <Dialog
+                <Modal
                     title="新增用户"
-                    size="tiny"
                     visible={this.state.dialogVisible}
+                    onOk={this.saveData.bind(this)}
                     onCancel={() => this.setState({ dialogVisible: false })}
-                    lockScroll={false}
                 >
-                    <Dialog.Body>
-                        <Form ref="form" rules={this.state.rules} model={this.state.form} labelWidth="0" style={{ textAlign: "center" }}>
-                            <Form.Item prop="username" label="">
+                    <Form ref="form" className="login-form">
+                        <Form.Item style={{ display: "none" }}>
+                            {getFieldDecorator('id', { initialValue: this.state.form.id }, {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('username', { initialValue: this.state.form.username }, {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
                                 <Input
-                                    style={{ width: "80%" }}
-                                    value={this.state.form.username}
-                                    onChange={this.onChange.bind(this, 'username')}
-                                    placeholder="请输入用户名"
-                                />
-                            </Form.Item>
-                            <Form.Item prop="password" label="">
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', { initialValue: this.state.form.password }, {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
                                 <Input
-                                    style={{ width: "80%" }}
-                                    value={this.state.form.password}
-                                    onChange={this.onChange.bind(this, 'password')}
-                                    placeholder="请输入密码"
-                                />
-                            </Form.Item>
-                        </Form>
-                    </Dialog.Body>
-                    <Dialog.Footer className="dialog-footer">
-                        <Button onClick={() => this.setState({ dialogVisible: false })}>取消</Button>
-                        <Button type="primary" onClick={this.saveData.bind(this)}>确定</Button>
-                    </Dialog.Footer>
-                </Dialog>
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Password"
+                                />,
+                            )}
+                        </Form.Item>
+                    </Form>
+                </Modal>
             </div>
         )
     }
 
+    /**
+     * 生命周期
+     * @author dwy
+     */
     componentDidMount() {
         $get("/api/persons").then(e => {
-            this.setState({ data: e.persons, page: { total: e.persons.length } })
-
+            // this.setState({ data: e.persons, page: { total: e.persons.length } })
+            this.setState({ data: e.persons, page: { total: e.persons.length, pageSize: 2 } })
         })
+        this.props.form.validateFields();
     }
 
+    /**
+     * 创建用户
+     * @author dwy
+     */
     createUser() {
         this.setState({
             dialogVisible: true, form: {
+                id: null,
                 username: '',
                 password: ''
             },
@@ -117,47 +157,78 @@ export default class Demo1 extends React.Component {
         })
     }
 
+    /**
+     * 更新用户
+     * @author dwy
+     */
     updateUser() {
         if (this.state.selection.length != 1) {
-            Message({ message: '请选择一条数据', type: 'info' });
+            message.info('请选择一条数据');
         } else {
             this.setState({ form: this.state.selection[0], state: 'update', dialogVisible: true })
         }
     }
 
+    /**
+     * 删除用户
+     * @author dwy
+     */
     deleteUser() {
         if (this.state.selection.length != 1) {
-            Message({ message: '请选择一条数据', type: 'info' });
+            message.info('请选择一条数据');
         } else {
             MessageBox.confirm('此操作将永久删除该用户, 是否继续?', '提示', {
                 type: 'warning'
             }).then(() => {
                 $post("/api/deletePerson", { id: this.state.selection[0].id }).then(e => {
                     this.setState({ dialogVisible: false })
-                    Message({ message: '成功', type: 'success' });
+                    message.success('删除成功');
                     this.componentDidMount();
                 })
-                Message({
-                    type: 'success',
-                    message: '删除成功!'
-                });
             })
         }
     }
 
-    onChange(key, value) {
-        this.setState({
-            form: Object.assign({}, this.state.form, { [key]: value })
-        });
-    }
-
+    /**
+     * 保存数据
+     * @author dwy
+     */
     saveData(e) {
-        $post(this.state.state == 'create' ? "/api/addPerson" : "/api/updatePerson", this.state.form).then(e => {
-            this.setState({ dialogVisible: false })
-            Message({ message: '成功', type: 'success' });
-            this.componentDidMount();
+        let userInfo = this.props.form.getFieldsValue();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                $post(this.state.state == 'create' ? "/api/addPerson" : "/api/updatePerson", userInfo).then(e => {
+                    this.setState({ dialogVisible: false })
+                    message.success('成功');
+                    this.componentDidMount();
+                })
+            }
         })
     }
 
+    /**
+     * 显示总条数
+     * @author dwy
+     */
+    showTotal() {
+        return `Total ${this.state.page.total} items`;
+    }
 
+    /**
+     * 分页页码修改方法
+     * @author dwy
+     */
+    changePage() {
+
+    }
+
+    /**
+     * 分页大小修改方法
+     * @author dwy
+     */
+    changePageSize() {
+
+    }
 }
+
+export default Form.create()(Demo1);
